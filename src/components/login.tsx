7083cd1fc7d3c69c0091f769b3892986411b0fcb
Humberto-Fajardo <hafajardo@unicauca.edu.co>
2024-04-28 12:09:48 -0500

    Reliace los ajustes deacuerdo al requerimiento y mejore las etiquetas de error

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -9,13 +9,17 @@ const Login = () => {
     // Función para manejar el envío del formulario
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        // Aquí puedes implementar la lógica para validar el inicio de sesión
-        // Por ahora, simplemente establecemos un mensaje de error si el nombre de usuario o la contraseña están vacíos
-        if (!username || !password) {
-            setError('Please enter username and password.');
+        // Validar si el nombre de usuario o la contraseña están vacíos
+        if (!username && !password) {
+            setError('Ingrese usuario y contraseña.');
+        } else if (!username) {
+            setError('Ingrese su nombre de usuario.');
+        } else if (!password) {
+            setError('Ingrese su contraseña.');
         } else {
+            // Aquí puedes implementar la lógica para validar el inicio de sesión
+            // Por ahora, simplemente establecemos un mensaje de error si no hay datos
             setError('');
-            // Aquí puedes enviar los datos del formulario a tu backend para autenticación
         }
     };
 
@@ -27,19 +31,15 @@ const Login = () => {
                     {/* Mostrar el mensaje de error si existe */}
                     {error && <div className="text-red-500 mb-4">{error}</div>}
                     <div className="mb-4">
+                        <label htmlFor="" className="block mt-2">Usuario</label>
                         <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="w-full p-2 rounded-md border border-gray-300 bg-gray-100" />
-                        <label htmlFor="" className="block mt-2">User name</label>
                     </div>
                     <div className="mb-4">
+                        <label htmlFor="" className="block mt-2">Contraseña</label>
                         <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="w-full p-2 rounded-md border border-gray-300 bg-gray-100" />
-                        <label htmlFor="" className="block mt-2">Password</label>
-                    </div>
-                    <div className="flex items-center mb-4">
-                        <input type="checkbox" id="rememberMe" className="mr-2" />
-                        <label htmlFor="rememberMe" className="text-black">Remember Me</label>
                     </div>
                     <div className="text-center mb-4">
-                        <a href="#" className="text-blue-500">Forgot Password?</a>
+                        <a href="#" className="text-blue-500">Olvido su contraseña?</a>
                     </div>
                     <button type="submit" className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300">Login</button>
                 </form>
