bd7700d3dd5ec28be0763365ffbe69e4a382a336
Humberto-Fajardo <hafajardo@unicauca.edu.co>
2024-05-18 10:35:43 -0500

    Defino la interfaz dentro del componete verUser

--- frontend/src/components/user/verUser.tsx
+++ frontend/src/components/user/verUser.tsx
@@ -1,20 +1,8 @@
-// Definir una interfaz para las propiedades del componente User
-interface UserProps {
-    user: {
-        name: string;
-        last_name: string;
-        document_type: string;
-        document_number: string;
-        gender: string;
-        email: string;
-        phone: string;
-        birthDate: string;
-        state: boolean;
-    };
-}
+// IMPORT THE USER INTERFACE
+import { UserInterface } from "../../interfaces/userProps.interface";
 
 // Modificar la función User para recibir las propiedades user
-function User(props: UserProps) {
+function User(props: { user: UserInterface }) {
     // Obtener los datos del usuario de las propiedades
     const { user } = props;
 
@@ -24,15 +12,15 @@ function User(props: UserProps) {
             <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
                 <div className="col-span-1 md:col-span-1">
                     {/* Contenido de la primera columna */}
-                    <img className="mx-auto rounded-full w-32 h-32" src="./photoTest.jpeg" alt="Foto de perfil" />
+                    <img className="mx-auto rounded-full w-32 h-32" src={user.photo || "./photoTest.jpeg"} alt="Foto de perfil" />
                 </div>
                 <div className="col-span-1 md:col-span-2">
                     {/* Contenido de la segunda columna */}
                     <p><strong>Nombre Completo:</strong> {user.name} {user.last_name}</p>
-                    <p><strong>Documento:</strong> {user.document_type} {user.document_number}</p>
-                    <p><strong>Género:</strong> {user.gender}</p>
-                    <p><strong>Contacto:</strong> Correo: {user.email}, Teléfono: {user.phone}</p>
-                    <p><strong>Fecha de Nacimiento:</strong> {user.birthDate}</p>
+                    <p><strong>Número de Documento:</strong> {user.document_number}</p>
+                    <p><strong>Email:</strong> {user.email}</p>
+                    <p><strong>Teléfono:</strong> {user.phone || "No especificado"}</p>
+                    <p><strong>Fecha de Nacimiento:</strong> {user.birthdate?.toString() || "No especificada"}</p>
                     <p><strong>Estado:</strong> {user.state ? "Activo" : "Inactivo"}</p>
                 </div>
             </div>
